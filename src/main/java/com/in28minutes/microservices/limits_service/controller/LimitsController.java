a20a56df0bc38d7310fc0760f1045999b7b2ff03
AravindSaiP <aravindsaipanasa.preparation@gmail.com>
2024-12-25 12:04:30 +0530

    Reading properties from application.properties to a Configuration class and passing them as arguments to Limits. Step 03

--- src/main/java/com/in28minutes/microservices/limits_service/controller/LimitsController.java
+++ src/main/java/com/in28minutes/microservices/limits_service/controller/LimitsController.java
@@ -1,14 +1,20 @@
 package com.in28minutes.microservices.limits_service.controller;
 
 import com.in28minutes.microservices.limits_service.bean.Limit;
+import com.in28minutes.microservices.limits_service.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limit retrieveLimits(){
-        return new Limit(1, 1000);
+        return new Limit(configuration.getMinimum(),configuration.getMaximum());
+        //return new Limit(1, 1000);
     }
 }
